18b3b93662024c9d4dbfaf85bfb25070c23cb1dd
Abdullah Mahmood <aamahmood72@gmail.com>
2020-10-12 11:47:12 -0400

    Made parser more robust; added more test data

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 # Abdullah Mahmood, aam2vp
 # Flood Warning Systems 20-21
 
-import json, re
+import json, re, sqlite3
 
 
 file_name = "test_data.json"
@@ -10,32 +10,76 @@ with open(file_name, "r") as read_file:
 
 transformed_data = []
 
-# TODO: Make map patterns for the other ways its ordered OR just make it more robust
-map_pattern = re.compile(r"map\[displayName:.*unit:.*value:.*]")
+# map_pattern = re.compile(r"map\[displayName:.*unit:.*value:.*]")
+map_pattern = re.compile(r"map\[.*:.*:.*:.*]")
 for reading in data:
     transformed_reading = {}
 
     for field in reading:
         value = reading[field]
+        
         if isinstance(value, str) and re.match(map_pattern, value):
-            unit_idx = value.index("unit:")
-            value_idx = value.index("value:")
-            unit_value = value[unit_idx+5:value_idx-1]
-            value_value = value[value_idx+6:-1]
-            try:
-                value_value = float(value_value)
-            except ValueError:
-                pass
-            transformed_reading[field] = value_value
-            transformed_reading["{}_unit".format(field)] = unit_value
+            line_split = value[4:-1].split(":")
+            len_line_split = len(line_split)
+
+            for i in range(1, len_line_split):
+                if (i == 1):
+                    subfield = line_split[0]
+
+                # Converting the subfields in fields like battery voltage, temperature, pressure, etc.
+                if (i < len_line_split-1):
+                    subvalue_split = line_split[i].split()
+                    subvalue = " ".join(subvalue_split[:-1])
+                    next_subfield = subvalue_split[-1]
+                else:
+                    subvalue = line_split[i]
+
+                if subfield != "displayName": # Skipping displayName as a field, redundant?
+                    if subfield == "value":
+                        subfield = field
+                    else:
+                        subfield = field + "_" + subfield
+                    
+                    try:
+                        subvalue = float(subvalue)
+                    except ValueError:
+                        pass
+
+                    transformed_reading[subfield] = subvalue
+                # end if
+                
+                subfield = next_subfield
+            # end for
         else:
             transformed_reading[field] = value
         # end if
     transformed_data.append(transformed_reading)
     # end for
-
 # end for
 
-# Test output
-for item in transformed_data:
-    print(item)
+# # Test output
+# for item in transformed_data:
+#     print(item)
+
+##################################################
+##################################################
+
+# TODO: Check for all the fields that and see if they exist in the table, 
+# if not, for loop and add new column. Might be faster to do try/except and check for the 
+# col
+# SQLite3 Connectivity
+# conn = sqlite3.connect("C:/Users/Shoikut/OneDrive/Documents/2020 Fall/SYS 4053/TestDataDB.db")
+# c = conn.cursor()
+
+# table = "test"
+# row1 = transformed_data[0]
+# # c.execute("INSERT INTO {}".format(table,))
+# c.execute("INSERT INTO test(battery_voltage_unit, battery_voltage) VALUES ('mV', 2479);")
+
+# conn.commit()
+# conn.close()
+
+##################################################
+##################################################
+
+# print("done!")
