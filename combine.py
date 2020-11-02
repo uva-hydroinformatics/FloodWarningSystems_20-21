6fda61a61d07aa2cb67305e95f7b7b2a488b1d9e
Abdullah Mahmood <aamahmood72@gmail.com>
2020-11-02 08:40:21 -0500

    Prints data neater

--- combine.py
+++ combine.py
@@ -3,5 +3,5 @@ import http_get, payload_parser
 data = http_get.get_data()
 transformed_data = payload_parser.parse_data(data)
 
-print(data)
-print("done")
+for reading in transformed_data:
+    print(reading)
