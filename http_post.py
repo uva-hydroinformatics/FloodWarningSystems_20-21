476904ba18aac2c5804494c506259095ecd96a89
Abdullah Mahmood <aamahmood72@gmail.com>
2020-11-14 19:01:59 -0500

    Update http_post.py

--- http_post.py
+++ http_post.py
@@ -1,33 +1,49 @@
 import requests
 
-device_urls = {
-    # TODO: Get the integration url and fix the URLs below for proper HTTP calls for downlink
-    'pressure': 'https://bad17136f2a19347c38adc89286ada12.m.pipedream.net',
-    'weather': 'https://bad17136f2a19347c38adc89286ada12.m.pipedream.net'
-    # 'pressure' : 'https://ruchir_dl-pr-26_5100.data.thethingsnetwork.org/',
-    # 'weather' : 'https://ruchir_dl-atm-41_5245.data.thethingsnetwork.org/'
-    # 'pressure': 'https://ruchir_dl-pr-26_5100.data.thethingsnetwork.org/api/v2/devices/=ttn-account-v2._mPeoCyPkrWW0v5IQZXCOIkHMItGVAM3oQ2saSr_0-4'
+
+device_ids = {
+    'pressure': 'dl-pr-26_5100',
+    'weather': ''
 }
 
+# TODO: Cleanup device urls (ask Abdullah for pressure device url)
+device_urls = {
+    'pressure': '',
+    'weather': ''
+}
+
+# Command set period + save
 sampling_rate = {
     # Sampling rate in seconds, encoded in hex
+    # Command generated from https://htmlpreview.github.io/?https://github.com/decentlab/decentlab-decoders/blob/master/downlink-command-encoder.html 
     # 60: '0002003CF5A1',
     # 300: '0002012CA9A1',
     # 900: '0002038477A1'
 
     # Sampling rate in seconds, encoded in base64 from hex
-    60: 'AAIAPPWh==',
-    300: 'AAIBLKmh==',
-    900: 'AAIDhHeh=='
+    60: 'AAIAPPWh',
+    300: 'AAIBLKmh',
+    600: 'AAICWH6h',
+    900: 'AAIDhHeh'
 }
 
-# TODO: Change function definition to take in device ID instead
+# TODO: Change function definition to take in device IDs instead?
 def update_sampling_period(device_url, seconds):
 
-    message = '{{"dev_id":"{}","payload_raw":"{}"}}'.format("dl-pr26_5100",sampling_rate[60])
+    message = '{{"dev_id":"{}","payload_raw":"{}"}}'.format(device_ids['pressure'], sampling_rate[seconds])
 
     downlink_command = requests.post(device_url, data=message)
 
     print(downlink_command.text)
 
-update_sampling_period(device_urls['pressure'], 60)
+# 1 minute sampling period
+# update_sampling_period(device_urls['pressure'], 60)
+
+# 5 minute sampling period
+# update_sampling_period(device_urls['pressure'], 300)
+
+# 10 minute sampling period
+update_sampling_period(device_urls['pressure'], 600)
+
+# 15 minute sampling period
+# update_sampling_period(device_urls['pressure'], 900)
