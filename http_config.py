2388939ef204c6391fcdd13d6333c962d7bdc1a1
Abdullah Mahmood <aamahmood72@gmail.com>
2020-11-23 02:25:19 -0500

    delete config file

--- http_config.py
+++
@@ -1,4 +0,0 @@
-# Downlink urls for devices
-
-pressure_url = 'https://integrations.thethingsnetwork.org/ttn-us-west/api/v2/down/uva_dl-pr-26_5100/uva_pressure-sensor?key=ttn-account-v2.Ts1TUTZRjl7oF2hpNjwvWo4cneohQCVTkQf-xRomovw'
-weather_url = 'https://integrations.thethingsnetwork.org/ttn-us-west/api/v2/down/ruchir_dl-atm-41_5245/weather-sensor?key=ttn-account-v2.5_nR04mHxTeiJWhtQiPjmN8ZN4PCtcUmOrohm5rUsNU'
