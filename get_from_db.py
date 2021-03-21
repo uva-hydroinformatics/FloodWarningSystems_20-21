8d27c59935ed8269269664b38064288357c3ca00
Kwadwo Tenkorang <knt4xx@virginia.edu>
2021-03-21 08:16:49 -0400

    get sensor readings from MySQL

---
+++ get_from_db.py
@@ -0,0 +1,100 @@
+import rds_config, pymysql, json
+import pandas as pd
+from datetime import datetime, date, timezone, timedelta
+from sshtunnel import SSHTunnelForwarder
+
+
+# Config for SSH Tunnel
+SSH_host = rds_config.floodDBcluster_endpoint
+SSH_user = rds_config.SSH_user
+SSH_password = rds_config.SSH_password
+SSH_private_key = ''  # add private key
+
+# Config for Flood DB Cluster
+db_host = rds_config.floodDBcluster_endpoint
+db_user = rds_config.floodDBcluster_username
+db_pass = rds_config.floodDBcluster_password
+db_name = rds_config.floodDB_name
+char_set = 'utf8mb4'
+
+def format_variable(var):
+    var_as_list = var.split('_')
+    var_part = var_as_list[2:] # assumes first 2 words in variable name are sensor type i.e. sensor_name_variable
+    return "_".join([str(x) for x in var_part])
+
+# TODO: make 'last' parameter work for hour and week i.e '2h' or '3w'
+def db_read(device, last='1d', start_date=None, end_date=None):
+
+    # Convert start_date into type datetime
+    if start_date is None:
+        datetime_start_date = datetime.now(timezone.utc)
+        if last is not None:
+            number_of_days = int(last[:-1])
+            datetime_start_date -= timedelta(days=number_of_days)
+        start_date = datetime.strftime(datetime_start_date, '%Y-%m-%d')
+    datetime_start_date = datetime.fromisoformat(start_date)
+
+    # Convert end_date into type datetime
+    if end_date is None:
+        datetime_end_date = datetime.now(timezone.utc)
+        end_date = datetime.strftime(datetime_end_date, '%Y-%m-%d')
+    datetime_end_date = datetime.fromisoformat(end_date)
+
+    # Create connection
+    with SSHTunnelForwarder(
+                            (SSH_host, 22),
+                            ssh_username=SSH_user,
+                            ssh_password=SSH_password,
+                            ssh_pkey=SSH_private_key,
+                            remote_bind_address=('127.0.0.1', 3306)) as SSH_server:
+        
+        connection = pymysql.connect(host='127.0.0.1',
+                            user=db_user,
+                            password=db_pass,
+                            port=SSH_server.local_bind_port,
+                            db=db_name,
+                            charset=char_set,
+                            cursorclass=pymysql.cursors.DictCursor)
+
+        try:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM `flood`.Values WHERE serial_ID='{}' AND datetime between '{}' and '{}'".format(device, str(datetime_start_date), str(datetime_end_date))
+                cursor.execute(sql)
+                result = cursor.fetchall()
+                
+                print(str(datetime_start_date), str(datetime_end_date))
+                if len(result) == 0:
+                    return []
+                
+                
+                
+                df = pd.DataFrame(result)
+                df['variable_ID'] = df.apply(lambda x: format_variable(x['variable_ID']), axis=1)
+
+                dates = df['datetime'].unique()
+                variables = df['variable_ID'].unique()
+                dfx = pd.DataFrame(index=dates, columns=variables)
+                
+                for index, x in df.iterrows():
+                    dfx.at[x['datetime'],  x['variable_ID']] = x['value']
+
+                dfx.reset_index(inplace=True)
+                dfx.rename(columns={"index":"datetime"}, inplace=True)
+                dfx['datetime'] = dfx['datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
+       
+ 
+                payload = json.loads(dfx.to_json(orient='records', date_format='iso'))
+                return json.dumps(payload, indent=4)
+                #print(json.dumps(payload, indent=4))
+                #print("done")
+                
+
+        except Exception as e:
+            print("Exception occured: {}".format(e))
+
+        finally:
+            connection.close()
+        
+
+
+# print(db_read('dl-atm41_5245', last='5d'))
