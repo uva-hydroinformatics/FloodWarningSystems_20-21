9eb544abd5c6af32cd8897818a321484186f7d47
Abdullah Mahmood <aamahmood72@gmail.com>
2021-04-05 00:38:06 -0400

    updated querySensors Lambda to be cleaner

--- AWS Lambda/lambda_function.py
+++ AWS Lambda/lambda_function.py
@@ -7,31 +7,22 @@ def lambda_handler(event, context):
     # latest readings period in hours (h), minutes (m), or seconds (s)
     reading_period = '90m'
 
-    print("Pressure Start")
-    pressure_data_1h = http_get.get_data('pressure', reading_period)  # returns data from pressure sensor
-    transformed_pressure_data = payload_parser.parse_data(pressure_data_1h)
-    upload.json_to_s3(transformed_pressure_data)
-    upload.update_db(transformed_pressure_data)
-    print("Pressure Done")
+    device_list = [
+        'dl-pr-26_5100',
+        'dl-atm41_5245',
+        'dl-mbx_5248',
+        'dl-mbx_5249'
+    ]
     
-    print("Weather Start")
-    weather_data_1h = http_get.get_data('weather', reading_period)  # returns data from weather sensor
-    transformed_weather_data = payload_parser.parse_data(weather_data_1h)
-    upload.json_to_s3(transformed_weather_data)
-    upload.update_db(transformed_weather_data)
-    print("Weather Done")
-    
-    print("Ultrasonic Start")
-    ultrasonic1_data_1h = http_get.get_data('ultrasonic1', reading_period)  # returns data from ultrasonic sensor 1 (bradywine bridge)
-    transformed_ultrasonic1_data = payload_parser.parse_data(ultrasonic1_data_1h)
-    upload.json_to_s3(transformed_ultrasonic1_data)
-    upload.update_db(transformed_ultrasonic1_data)
-    
-    ultrasonic2_data_1h = http_get.get_data('ultrasonic2', reading_period)  # returns data from ultrasonic sensor 2 (greenbrier bridge)
-    transformed_ultrasonic2_data = payload_parser.parse_data(ultrasonic2_data_1h)
-    upload.json_to_s3(transformed_ultrasonic2_data)
-    upload.update_db(transformed_ultrasonic2_data)
-    print("Ultrasonic Done")
+    for device in device_list:
+        device_payload = http_get.get_data(device, reading_period)
+        transformed_sensor_data = payload_parser.parse_data(device_payload)
+        try:
+            upload.update_db(device=device, data=transformed_sensor_data, ssh_connection=True)
+        except:
+            print("Could not update database for device: {}".format(device))
+        upload.json_to_s3(transformed_sensor_data)
+        
 
     return {
         'statusCode': 200,
